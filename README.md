676d82a86d3914ff8c03705501ecc6d8852a2c69
calintat <calintat@gmail.com>
2017-03-15 22:47:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Colorful
+
+[![](https://jitpack.io/v/calintat/colorful.svg)](https://jitpack.io/#calintat/alps)
+
+Get quick access to all Material Design colors in both Java and XML.
+
+How to use
+----------
+
+Add the JitPack maven repository to the list of repositories:
+
+```gradle
+allprojects {
+    repositories {
+        maven { url "https://jitpack.io" }
+    }
+}
+```
+
+Add the dependency to the gradle build file:
+
+```gradle
+dependencies {
+    compile 'com.github.calintat:colorful:1.0'
+}
+```
+
+Now you have access to the entire Material Design color palette:
+
+```java
+coordinatorLayout.setStatusBarColor(Colorful.blue700);
+```
+
+```xml
+<style name="AppTheme" parent="Theme.AppCompat.Light.DarkActionBar">
+    <item name="colorPrimary">@color/blue_500</item>
+    <item name="colorPrimaryDark">@color/blue_700</item>
+    <item name="colorAccent">@color/yellow_A200</item>
+</style>
+```
---
+++ library/src/main/java/com/github/calintat/Colorful.java
@@ -0,0 +1,339 @@
+package com.github.calintat;
+
+public final class Colorful {
+
+    /* RED */
+
+    public static int red050 = 0xffebee;
+    public static int red100 = 0xffcdd2;
+    public static int red200 = 0xef9a9a;
+    public static int red300 = 0xe57373;
+    public static int red400 = 0xef5350;
+    public static int red500 = 0xf44336;
+    public static int red600 = 0xe53935;
+    public static int red700 = 0xd32f2f;
+    public static int red800 = 0xc62828;
+    public static int red900 = 0xb71c1c;
+
+    public static int redA100 = 0xff8a80;
+    public static int redA200 = 0xff5252;
+    public static int redA400 = 0xff1744;
+    public static int redA700 = 0xd50000;
+
+    /* PINK */
+
+    public static int pink050 = 0xfce4ec;
+    public static int pink100 = 0xf8bbd0;
+    public static int pink200 = 0xf48fb1;
+    public static int pink300 = 0xf06292;
+    public static int pink400 = 0xec407a;
+    public static int pink500 = 0xe91e63;
+    public static int pink600 = 0xd81b60;
+    public static int pink700 = 0xc2185b;
+    public static int pink800 = 0xad1457;
+    public static int pink900 = 0x880e4f;
+
+    public static int pinkA100 = 0xff80ab;
+    public static int pinkA200 = 0xff4081;
+    public static int pinkA400 = 0xf50057;
+    public static int pinkA700 = 0xc51162;
+
+    /* PURPLE */
+
+    public static int purple050 = 0xf3e5f5;
+    public static int purple100 = 0xe1bee7;
+    public static int purple200 = 0xce93d8;
+    public static int purple300 = 0xba68c8;
+    public static int purple400 = 0xab47bc;
+    public static int purple500 = 0x9c27b0;
+    public static int purple600 = 0x8e24aa;
+    public static int purple700 = 0x7b1fa2;
+    public static int purple800 = 0x6a1b9a;
+    public static int purple900 = 0x4a148c;
+
+    public static int purpleA100 = 0xea80fc;
+    public static int purpleA200 = 0xe040fb;
+    public static int purpleA400 = 0xd500f9;
+    public static int purpleA700 = 0xaa00ff;
+
+    /* DEEP PURPLE */
+
+    public static int deepPurple050 = 0xede7f6;
+    public static int deepPurple100 = 0xd1c4e9;
+    public static int deepPurple200 = 0xb39ddb;
+    public static int deepPurple300 = 0x9575cd;
+    public static int deepPurple400 = 0x7e57c2;
+    public static int deepPurple500 = 0x673ab7;
+    public static int deepPurple600 = 0x5e35b1;
+    public static int deepPurple700 = 0x512da8;
+    public static int deepPurple800 = 0x4527a0;
+    public static int deepPurple900 = 0x311b92;
+
+    public static int deepPurpleA100 = 0xb388ff;
+    public static int deepPurpleA200 = 0x7c4dff;
+    public static int deepPurpleA400 = 0x651fff;
+    public static int deepPurpleA700 = 0x6200ea;
+
+    /* INDIGO */
+
+    public static int indigo050 = 0xe8eaf6;
+    public static int indigo100 = 0xc5cae9;
+    public static int indigo200 = 0x9fa8da;
+    public static int indigo300 = 0x7986cb;
+    public static int indigo400 = 0x5c6bc0;
+    public static int indigo500 = 0x3f51b5;
+    public static int indigo600 = 0x3949ab;
+    public static int indigo700 = 0x303f9f;
+    public static int indigo800 = 0x283593;
+    public static int indigo900 = 0x1a237e;
+
+    public static int indigoA100 = 0x8c9eff;
+    public static int indigoA200 = 0x536dfe;
+    public static int indigoA400 = 0x3d5afe;
+    public static int indigoA700 = 0x304ffe;
+
+    /* BLUE */
+
+    public static int blue050 = 0xe3f2fd;
+    public static int blue100 = 0xbbdefb;
+    public static int blue200 = 0x90caf9;
+    public static int blue300 = 0x64b5f6;
+    public static int blue400 = 0x42a5f5;
+    public static int blue500 = 0x2196f3;
+    public static int blue600 = 0x1e88e5;
+    public static int blue700 = 0x1976d2;
+    public static int blue800 = 0x1565c0;
+    public static int blue900 = 0x0d47a1;
+
+    public static int blueA100 = 0x82b1ff;
+    public static int blueA200 = 0x448aff;
+    public static int blueA400 = 0x2979ff;
+    public static int blueA700 = 0x2962ff;
+
+    /* LIGHT BLUE */
+
+    public static int lightBlue050 = 0xe1f5fe;
+    public static int lightBlue100 = 0xb3e5fc;
+    public static int lightBlue200 = 0x81d4fa;
+    public static int lightBlue300 = 0x4fc3f7;
+    public static int lightBlue400 = 0x29b6f6;
+    public static int lightBlue500 = 0x03a9f4;
+    public static int lightBlue600 = 0x039be5;
+    public static int lightBlue700 = 0x0288d1;
+    public static int lightBlue800 = 0x0277bd;
+    public static int lightBlue900 = 0x01579b;
+
+    public static int lightBlueA100 = 0x80d8ff;
+    public static int lightBlueA200 = 0x40c4ff;
+    public static int lightBlueA400 = 0x00b0ff;
+    public static int lightBlueA700 = 0x0091ea;
+
+    /* CYAN */
+
+    public static int cyan050 = 0xe0f7fa;
+    public static int cyan100 = 0xb2ebf2;
+    public static int cyan200 = 0x80deea;
+    public static int cyan300 = 0x4dd0e1;
+    public static int cyan400 = 0x26c6da;
+    public static int cyan500 = 0x00bcd4;
+    public static int cyan600 = 0x00acc1;
+    public static int cyan700 = 0x0097a7;
+    public static int cyan800 = 0x00838f;
+    public static int cyan900 = 0x006064;
+
+    public static int cyanA100 = 0x84ffff;
+    public static int cyanA200 = 0x18ffff;
+    public static int cyanA400 = 0x00e5ff;
+    public static int cyanA700 = 0x00b8d4;
+
+    /* TEAL */
+
+    public static int teal050 = 0xe0f2f1;
+    public static int teal100 = 0xb2dfdb;
+    public static int teal200 = 0x80cbc4;
+    public static int teal300 = 0x4db6ac;
+    public static int teal400 = 0x26a69a;
+    public static int teal500 = 0x009688;
+    public static int teal600 = 0x00897b;
+    public static int teal700 = 0x00796b;
+    public static int teal800 = 0x00695c;
+    public static int teal900 = 0x004d40;
+
+    public static int tealA100 = 0xa7ffeb;
+    public static int tealA200 = 0x64ffda;
+    public static int tealA400 = 0x1de9b6;
+    public static int tealA700 = 0x00bfa5;
+
+    /* GREEN */
+
+    public static int green050 = 0xe8f5e9;
+    public static int green100 = 0xc8e6c9;
+    public static int green200 = 0xa5d6a7;
+    public static int green300 = 0x81c784;
+    public static int green400 = 0x66bb6a;
+    public static int green500 = 0x4caf50;
+    public static int green600 = 0x43a047;
+    public static int green700 = 0x388e3c;
+    public static int green800 = 0x2e7d32;
+    public static int green900 = 0x1b5e20;
+
+    public static int greenA100 = 0xb9f6ca;
+    public static int greenA200 = 0x69f0ae;
+    public static int greenA400 = 0x00e676;
+    public static int greenA700 = 0x00c853;
+
+    /* LIGHT GREEN */
+
+    public static int lightGreen050 = 0xf1f8e9;
+    public static int lightGreen100 = 0xdcedc8;
+    public static int lightGreen200 = 0xc5e1a5;
+    public static int lightGreen300 = 0xaed581;
+    public static int lightGreen400 = 0x9ccc65;
+    public static int lightGreen500 = 0x8bc34a;
+    public static int lightGreen600 = 0x7cb342;
+    public static int lightGreen700 = 0x689f38;
+    public static int lightGreen800 = 0x558b2f;
+    public static int lightGreen900 = 0x33691e;
+
+    public static int lightGreenA100 = 0xccff90;
+    public static int lightGreenA200 = 0xb2ff59;
+    public static int lightGreenA400 = 0x76ff03;
+    public static int lightGreenA700 = 0x64dd17;
+
+    /* LIME */
+
+    public static int lime050 = 0xf9fbe7;
+    public static int lime100 = 0xf0f4c3;
+    public static int lime200 = 0xe6ee9c;
+    public static int lime300 = 0xdce775;
+    public static int lime400 = 0xd4e157;
+    public static int lime500 = 0xcddc39;
+    public static int lime600 = 0xc0ca33;
+    public static int lime700 = 0xafb42b;
+    public static int lime800 = 0x9e9d24;
+    public static int lime900 = 0x827717;
+
+    public static int limeA100 = 0xf4ff81;
+    public static int limeA200 = 0xeeff41;
+    public static int limeA400 = 0xc6ff00;
+    public static int limeA700 = 0xaeea00;
+
+    /* YELLOW */
+
+    public static int yellow050 = 0xfffde7;
+    public static int yellow100 = 0xfff9c4;
+    public static int yellow200 = 0xfff59d;
+    public static int yellow300 = 0xfff176;
+    public static int yellow400 = 0xffee58;
+    public static int yellow500 = 0xffeb3b;
+    public static int yellow600 = 0xfdd835;
+    public static int yellow700 = 0xfbc02d;
+    public static int yellow800 = 0xf9a825;
+    public static int yellow900 = 0xf57f17;
+
+    public static int yellowA100 = 0xffff8d;
+    public static int yellowA200 = 0xffff00;
+    public static int yellowA400 = 0xffea00;
+    public static int yellowA700 = 0xffd600;
+
+    /* AMBER */
+
+    public static int amber050 = 0xfff8e1;
+    public static int amber100 = 0xffecb3;
+    public static int amber200 = 0xffe082;
+    public static int amber300 = 0xffd54f;
+    public static int amber400 = 0xffca28;
+    public static int amber500 = 0xffc107;
+    public static int amber600 = 0xffb300;
+    public static int amber700 = 0xffa000;
+    public static int amber800 = 0xff8f00;
+    public static int amber900 = 0xff6f00;
+
+    public static int amberA100 = 0xffe57f;
+    public static int amberA200 = 0xffd740;
+    public static int amberA400 = 0xffc400;
+    public static int amberA700 = 0xffab00;
+
+    /* ORANGE */
+
+    public static int orange050 = 0xfff3e0;
+    public static int orange100 = 0xffe0b2;
+    public static int orange200 = 0xffcc80;
+    public static int orange300 = 0xffb74d;
+    public static int orange400 = 0xffa726;
+    public static int orange500 = 0xff9800;
+    public static int orange600 = 0xfb8c00;
+    public static int orange700 = 0xf57c00;
+    public static int orange800 = 0xef6c00;
+    public static int orange900 = 0xe65100;
+
+    public static int orangeA100 = 0xffd180;
+    public static int orangeA200 = 0xffab40;
+    public static int orangeA400 = 0xff9100;
+    public static int orangeA700 = 0xff6d00;
+
+    /* DEEP ORANGE */
+
+    public static int deepOrange050 = 0xfbe9e7;
+    public static int deepOrange100 = 0xffccbc;
+    public static int deepOrange200 = 0xffab91;
+    public static int deepOrange300 = 0xff8a65;
+    public static int deepOrange400 = 0xff7043;
+    public static int deepOrange500 = 0xff5722;
+    public static int deepOrange600 = 0xf4511e;
+    public static int deepOrange700 = 0xe64a19;
+    public static int deepOrange800 = 0xd84315;
+    public static int deepOrange900 = 0xbf360c;
+
+    public static int deepOrangeA100 = 0xff9e80;
+    public static int deepOrangeA200 = 0xff6e40;
+    public static int deepOrangeA400 = 0xff3d00;
+    public static int deepOrangeA700 = 0xdd2c00;
+
+    /* BROWN */
+
+    public static int brown050 = 0xefebe9;
+    public static int brown100 = 0xd7ccc8;
+    public static int brown200 = 0xbcaaa4;
+    public static int brown300 = 0xa1887f;
+    public static int brown400 = 0x8d6e63;
+    public static int brown500 = 0x795548;
+    public static int brown600 = 0x6d4c41;
+    public static int brown700 = 0x5d4037;
+    public static int brown800 = 0x4e342e;
+    public static int brown900 = 0x3e2723;
+
+    /* GREY */
+
+    public static int grey050 = 0xfafafa;
+    public static int grey100 = 0xf5f5f5;
+    public static int grey200 = 0xeeeeee;
+    public static int grey300 = 0xe0e0e0;
+    public static int grey400 = 0xbdbdbd;
+    public static int grey500 = 0x9e9e9e;
+    public static int grey600 = 0x757575;
+    public static int grey700 = 0x616161;
+    public static int grey800 = 0x424242;
+    public static int grey900 = 0x212121;
+
+    /* BLUE GREY */
+
+    public static int blueGrey050 = 0xeceff1;
+    public static int blueGrey100 = 0xcfd8dc;
+    public static int blueGrey200 = 0xb0bec5;
+    public static int blueGrey300 = 0x90a4ae;
+    public static int blueGrey400 = 0x78909c;
+    public static int blueGrey500 = 0x607d8b;
+    public static int blueGrey600 = 0x546e7a;
+    public static int blueGrey700 = 0x455a64;
+    public static int blueGrey800 = 0x37474f;
+    public static int blueGrey900 = 0x263238;
+
+    /* BLACK */
+
+    public static int black = 0x000000;
+
+    /* WHITE */
+
+    public static int white = 0xffffff;
+}
